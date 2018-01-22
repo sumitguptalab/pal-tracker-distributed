e471a7394580dc140de0e9c6fa98df61aaf9571d
Sumit Gupta <sumit.gupta@kroger.com>
2018-01-22 17:05:14 -0500

    Securing a distributed system Lab -- added Redis stuf

--- applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
+++ applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/App.java
@@ -1,5 +1,6 @@
 package io.pivotal.pal.tracker.allocations;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,9 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.client.RestOperations;
@@ -31,6 +35,26 @@ public class App {
         RestOperations restOperations,
         @Value("${registration.server.endpoint}") String registrationEndpoint
     ) {
-        return new ProjectClient(restOperations, registrationEndpoint);
+        return new ProjectClient(restOperations, registrationEndpoint, redisTemplate());
+    }
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+
+        return new JedisConnectionFactory();
+
+    }
+
+    @Bean
+    @Qualifier("MyRedisTemplate")
+    public RedisTemplate<String, Object> redisTemplate() {
+        final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+
+        template.setConnectionFactory(jedisConnectionFactory());
+
+        template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+
+        return template;
+
     }
 }
--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,10 +1,13 @@
 package io.pivotal.pal.tracker.allocations;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.client.RestOperations;
 
 import java.util.Map;
@@ -19,60 +22,58 @@ public class ProjectClient {
 
 
     @Autowired
-    private StringRedisTemplate template;
+    @Qualifier("MyRedisTemplate")
+    private RedisTemplate template;
 
     private Logger logger = LoggerFactory.getLogger(this.getClass().getName());
 
-    public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
+    public ProjectClient(RestOperations restOperations, String registrationServerEndpoint, final RedisTemplate<String, Object> redisTemplate) {
         this.restOperations = restOperations;
         this.registrationServerEndpoint = registrationServerEndpoint;
+        this.template = redisTemplate;
     }
 
-    @HystrixCommand(fallbackMethod = "getProjectFromInMemoryCache")
-    public ProjectInfo getProject(long projectId) {
-        logger.info("Calling real service for projectId : " + projectId);
-        ProjectInfo projectInfo = restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
-        logger.info("Updating cache for projectId : " + projectId);
-        projectsCache.put(projectId, projectInfo);
-        return projectsCache.get(projectId);
-    }
-
-    public ProjectInfo getProjectFromInMemoryCache(long projectId) {
-        logger.info("reading from Cache for projectId : " + projectId);
-        return projectsCache.get(projectId);
-    }
-
-//    @HystrixCommand(fallbackMethod = "getProjectFromRedisCache")
+//    @HystrixCommand(fallbackMethod = "getProjectFromInMemoryCache")
 //    public ProjectInfo getProject(long projectId) {
-//
-//        try {
-//
-//            ObjectMapper objectMapper = new ObjectMapper();
-//            logger.info("Calling real service for projectId : " + projectId);
-//            ProjectInfo projectInfo = restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
-//            logger.info("Updating cache for projectId : " + projectId);
-//
-//            ValueOperations<String, String> ops = this.template.opsForValue();
-//            if (!this.template.hasKey(String.valueOf(projectId))) {
-//
-//                ops.set(String.valueOf(projectId), objectMapper.writeValueAsString(projectInfo));
-//            }
-//
-//            return objectMapper.readValue(ops.get(String.valueOf(projectId)), ProjectInfo.class);
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
+//        logger.info("Calling real service for projectId : " + projectId);
+//        ProjectInfo projectInfo = restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+//        logger.info("Updating cache for projectId : " + projectId);
+//        projectsCache.put(projectId, projectInfo);
+//        return projectsCache.get(projectId);
 //    }
-//
-//    public ProjectInfo getProjectFromRedisCache(long projectId) throws Exception {
-//        try {
-//            logger.info("reading from Redis Cache for projectId : " + projectId);
-//            ObjectMapper objectMapper = new ObjectMapper();
-//            ValueOperations<String, String> ops = this.template.opsForValue();
-//            return objectMapper.readValue(ops.get(String.valueOf(projectId)), ProjectInfo.class);
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
+
+//    public ProjectInfo getProjectFromInMemoryCache(long projectId) {
+//        logger.info("reading from Cache for projectId : " + projectId);
+//        return projectsCache.get(projectId);
 //    }
+
+    @HystrixCommand(fallbackMethod = "getProjectFromRedisCache")
+    public ProjectInfo getProject(long projectId) {
+
+        try {
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            logger.info("Calling real service for projectId : " + projectId);
+            ProjectInfo projectInfo = restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
+            logger.info("Updating cache for projectId : " + projectId);
+
+            ValueOperations<String, String> ops = this.template.opsForValue();
+            if (!this.template.hasKey(String.valueOf(projectId))) {
+
+                ops.set(String.valueOf(projectId), objectMapper.writeValueAsString(projectInfo));
+            }
+
+            return objectMapper.readValue(ops.get(String.valueOf(projectId)), ProjectInfo.class);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ProjectInfo getProjectFromRedisCache(long projectId) throws Exception {
+        logger.info("reading from Redis Cache for projectId : " + projectId);
+        ObjectMapper objectMapper = new ObjectMapper();
+        ValueOperations<String, String> ops = this.template.opsForValue();
+        return objectMapper.readValue(ops.get(String.valueOf(projectId)), ProjectInfo.class);
+    }
 
 }
